8e83676e80e66e52e877eda53264e5b5291b1638
abdulaziz <aziz@marensolutions>
2021-09-15 12:48:37 +0530

    updated js component

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,25 @@
 import { NativeModules } from 'react-native';
 
+/**
+ * 
+ * @param iconName 
+ * @returns 
+ * 
+ * #### Reset Primary App Icon Example
+ *
+ * Pass empty string to reset Original App Icon
+ * ```js
+ * changeIcon(""); || changeIcon();
+ * ```
+ *
+ * #### Change App Icon Example
+ *
+ * Pass empty string to reset Original App Icon
+ * ```js
+ * changeIcon("ExampleIcon1");
+ * ```
+ * 
+ */
 const changeIcon = (iconName: string): Promise<string> =>
   NativeModules.ChangeIcon.changeIcon(iconName);
 
